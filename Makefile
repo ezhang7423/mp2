2e163a84bca90f4a28fe0bc5e3d5184c0e47eb2f
ezhang7423 <ezhang7423@student.palomar.edu>
2020-05-16 22:13:35 -0700

    test matrix

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 main:
 	g++ main.cpp -o gobang
 optimized:
-	g++ -march=native -Ofast -frename-registers -fopenmp -D_GLIBCXX_PARALLEL main.cpp
+	g++ -march=native -Ofast -frename-registers -fopenmp -D_GLIBCXX_PARALLEL main.cpp -o gobang
 dev:
 	nodemon -x "make main && ./gobang || exit 1" main.cpp
--- matrix.cpp
+++ matrix.cpp
@@ -1,5 +1,20 @@
-#include <vector>
-
-class matrix
-{
+#include <iostream>
+using namespace std;
+int main(){
+    int **arr = new int *[300];
+    cout <<  arr[0] << endl;
+    for (int i = 0; i < 300; i++){
+        arr[i] = new int[300];
+    }
+    for (int i = 0; i < 300; i++){
+        for (int j = 0 ; j < 300; j++){
+            arr[i][j] = 3 * i + j;
+        }
+    }
+    for (int i = 0; i < 300; i++){
+        for (int j = 0 ; j < 300; j++){
+            cout << arr[i][j] << " ";
+        }
+    }
+    
 }
