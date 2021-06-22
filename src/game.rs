2e8f837dcb6adf5e4301301675eb9a759f6f5d56
ezhang7423 <ezhang7423@student.palomar.edu>
2021-06-22 12:42:44 -0700

    rename vars

--- src/game.rs
+++ src/game.rs
@@ -1,20 +1,20 @@
 use ndarray::{Array, Ix2};
 
 enum GameCondition {
-    not_started,
-    in_progress,
-    finished,
+    NotStarted,
+    InProgress,
+    Finished,
 }
 #[derive(Copy, Clone)]
 enum Player {
-    human = 0,
-    bot = 1,
+    Human = 0,
+    Bot = 1,
 }
 pub struct Game {
     pub size: usize,
     condition: GameCondition,
-    black: Player,
-    white: Player,
+    // black: Player,
+    // white: Player,
     current_player: Player,
     waiting_player: Player,
     state: Array<usize, Ix2>,
@@ -22,13 +22,13 @@ pub struct Game {
 
 impl Game {
     fn finished(&self) -> bool {
-        // check if finished
-        let finished = false;
+        // check if Finished
+        let Finished = false;
 
-        finished
+        Finished
     }
     fn get_move(&self) -> (usize, usize) {
-        if matches!(self.current_player, Player::human) {
+        if matches!(self.current_player, Player::Human) {
             println!("What's your move?")
         } else {
             // idk
@@ -37,11 +37,11 @@ impl Game {
     }
 
     pub fn start(&mut self) {
-        if matches!(self.condition, GameCondition::not_started) {
+        if matches!(self.condition, GameCondition::NotStarted) {
             eprintln!("Game cannot be started in its current condition");
             return;
         }
-        self.condition = GameCondition::in_progress;
+        self.condition = GameCondition::InProgress;
 
         loop {
             self.print();
@@ -49,7 +49,7 @@ impl Game {
             self.state[player_move] = self.current_player as usize;
 
             if self.finished() {
-                self.condition = GameCondition::finished;
+                self.condition = GameCondition::Finished;
                 break;
             } else {
                 // swap current and waiting
@@ -63,23 +63,23 @@ impl Game {
     pub fn new(size: usize, human_first: Option<bool>) -> Self {
         let human_first = human_first.unwrap_or(true);
 
-        // assume that bot starts
+        // assume that Bot starts
         let (mut black, mut white, mut current_player, mut waiting_player) =
-            (Player::bot, Player::human, Player::bot, Player::human);
+            (Player::Bot, Player::Human, Player::Bot, Player::Human);
         if human_first {
-            black = Player::human;
-            white = Player::bot;
-            current_player = Player::human;
-            waiting_player = Player::bot
+            // black = Player::Human;
+            // white = Player::Bot;
+            current_player = Player::Human;
+            waiting_player = Player::Bot
         }
 
         let state = Array::<usize, _>::zeros((size, size));
 
         Self {
             size,
-            condition: GameCondition::not_started,
-            black,
-            white,
+            condition: GameCondition::NotStarted,
+            // black,
+            // white,
             current_player,
             waiting_player,
             state: state,
